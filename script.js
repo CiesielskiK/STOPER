39f20615d76ae1f53f6172ca347fb14da6c53868
Krzysztof Ciesielski <krzysztof.ciesielski@powertech.com.pl>
2018-08-10 12:05:07 +0200

    Change script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-/* jshint -W097 */
-
 class Stopwatch {
   constructor(display) {
     this.running = false;
     this.display = display;
     this.reset();
     this.print(this.times);
-    this.count = 0;
   }
 
   reset() {
@@ -31,13 +28,6 @@ class Stopwatch {
     this.print();
   }
 
-  start() {
-    if (!this.running) {
-      this.running = true;
-      this.watch = setInterval(() => this.step(), 10);
-    }
-  }
-
   calculate() {
     this.times.miliseconds += 1;
     if (this.times.miliseconds >= 100) {
@@ -45,8 +35,15 @@ class Stopwatch {
       this.times.miliseconds = 0;
     }
     if (this.times.seconds >= 60) {
-      this.times.minutes += 1;
-      this.times.seconds = 0;
+        this.times.minutes += 1;
+        this.times.seconds = 0;
+    }
+  }
+
+  start() {
+    if (!this.running) {
+      this.running = true;
+      this.watch = setInterval(() => this.step(), 10);
     }
   }
 
@@ -55,69 +52,46 @@ class Stopwatch {
     clearInterval(this.watch);
   }
 
-  addResult() {
-    if (!this.running) return;
-    let counter = this.count += 1;
-    let results = document.getElementById('results');
-    let times = this.format(this.times).toString();
-    results.insertAdjacentHTML('afterbegin',
-    '<li class="results"><p>'+ counter +'.</p> ' + times +'</li>');
+  resetTimer() {
+    this.times = {
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0
+    };
+    this.display.innerText = this.format(this.times);
+    document.getElementById('results').innerHTML = '';
   }
 
-  clearResult() {
-    let results = document.getElementById('results');
-    this.count = 0;
-    results.innerHTML = '';
-  }
+  save() {
+    let lapTime = document.getElementById('stopwatch').innerHTML;
+    console.log(lapTime);
 
-  splitResult() {
-    let results = document.getElementById('results');
-    if (results.firstChild.innerHTML === 'TIMER RESET') return;
-    results.insertAdjacentHTML('afterbegin', '<li class="results" style="color:red">TIMER RESET</li>');
+    const x = document.createElement('li');
+    const t = document.createTextNode(lapTime);
+    x.appendChild(t);
+    document.getElementById('results').appendChild(x);
   }
 }
 
 function pad0(value) {
   let result = value.toString();
   if (result.length < 2) {
-    result = '0' + result;
+     result = '0' + result;
   }
   return result;
 }
 
-const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
+const stopwatch = new Stopwatch(
+document.querySelector('.stopwatch'));
 
 let startButton = document.getElementById('start');
-startButton.addEventListener('click', () => {
-  startButton.style.visibility = "hidden";
-  stopButton.style.visibility = "visible";
-  stopwatch.start();
-});
-
+startButton.addEventListener('click', () => stopwatch.start());
 
 let stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => {
-  stopButton.style.visibility = "hidden";
-  startButton.style.visibility = "visible";
-  stopwatch.stop();
-});
+stopButton.addEventListener('click', () => stopwatch.stop());
 
 let resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', () => {
-  stopwatch.stop();
-  stopButton.style.visibility = "hidden";
-  startButton.style.visibility = "visible";
-  stopwatch.reset();
-  stopwatch.print();
-  stopwatch.splitResult();
-});
+resetButton.addEventListener('click', () => stopwatch.resetTimer());
 
-let resultButton = document.getElementById('result');
-resultButton.addEventListener('click', () => {
-  stopwatch.addResult();
-});
-
-let clearButton = document.getElementById('clear');
-clearButton.addEventListener('click', () => {
-  stopwatch.clearResult();
-});
+let saveButton = document.getElementById('save');
+saveButton.addEventListener('click', () => stopwatch.save());
